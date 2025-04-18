81bda8265b095625edd18947e7257ec789cc2183
Radmir <tejehdhe@gmail.com>
2025-04-18 18:21:57 +0300

    Base functional

---
+++ Koridor/Koridor/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Koridor
+{
+    public partial class MainWindow : Window
+    {
+        int cols = 9;
+        int rows = 9;
+        List<Line> vertLines = new List<Line>();
+        List<Line> horizontLines = new List<Line>();
+        double canvasWidth ;
+        double canvasHeight;
+        double cellWidth;
+        double cellHeight;
+        int DashMargin;
+        int DashLenght;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DrawGrid();
+        }
+
+        private void MainSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateGrid();
+        }
+
+        private void DrawGrid()
+        {
+            canvas.Children.Clear();
+            vertLines.Clear();
+            horizontLines.Clear();
+
+            canvasWidth = canvas.ActualWidth;
+            canvasHeight = canvas.ActualHeight;
+            cellWidth = canvasWidth / cols;
+            cellHeight = canvasHeight / rows;
+
+            DashMargin = (int)(cellHeight * 0.25);
+            DashLenght = (int)(cellHeight * 0.5);
+
+            // Вертикальные линии
+            for (int i = 0; i <= cols; i++)
+            {
+                Line line = new Line()
+                {
+                    X1 = cellWidth * i,
+                    Y1 = DashMargin,
+                    X2 = cellWidth * i,
+                    Y2 = canvasHeight,
+                    Stroke = Brushes.Red,
+                    StrokeThickness = 2,
+                    StrokeDashArray = new DoubleCollection() { DashLenght, DashMargin } // Задаем штриховые линии (длина линии, растояние между линиями)
+                };
+                canvas.Children.Add(line);
+                vertLines.Add(line);
+            }
+
+            // Горизонтальные линии
+            for (int i = 0; i <= rows; i++)
+            {
+                Line line = new Line()
+                {
+                    X1 = DashMargin,
+                    Y1 = cellHeight * i,
+                    X2 = canvasWidth,
+                    Y2 = cellHeight * i,
+                    Stroke = Brushes.Red,
+                    StrokeThickness = 2,
+                    StrokeDashArray =  new DoubleCollection() { DashLenght, DashMargin } // Задаем штриховые линии (длина линии, растояние между линиями)
+                };
+                canvas.Children.Add(line);
+                horizontLines.Add(line);
+            }
+        }
+
+        private void UpdateGrid()
+        {
+            canvasWidth = canvas.ActualWidth;
+            canvasHeight = canvas.ActualHeight;
+            cellWidth = canvasWidth / cols;
+            cellHeight = canvasHeight / rows;
+
+            DashMargin = (int)(cellHeight * 0.25);
+            DashLenght = (int)(cellHeight * 0.5);
+
+            // Обновление вертикальных линий
+            for (int i = 0; i < vertLines.Count; i++)
+            {
+                vertLines[i].X1 = cellWidth * i;
+                vertLines[i].X2 = cellWidth * i;
+                vertLines[i].Y2 = canvasHeight;
+                vertLines[i].StrokeDashArray = new DoubleCollection() { DashLenght, DashMargin };
+            }
+
+            // Обновление горизонтальных линий
+            for (int i = 0; i < horizontLines.Count; i++)
+            {
+                horizontLines[i].Y1 = cellHeight * i;
+                horizontLines[i].Y2 = cellHeight * i;
+                horizontLines[i].X2 = canvasWidth;
+                horizontLines[i].StrokeDashArray = new DoubleCollection() { DashLenght, DashMargin };
+            }
+        }
+    }
+}
